fe7e5d41bdb493f1d076f24635dc5385257d45c2
pankajvcm244 <pankajvcm244@gmail.com>
2025-01-22 11:58:28 +0000

    added item code printer support to print labels

--- vcm/erpnext_vcm/doctype/vcm_item_code_printer/vcm_item_code_printer.js
+++ vcm/erpnext_vcm/doctype/vcm_item_code_printer/vcm_item_code_printer.js
@@ -1,244 +1,101 @@
 // Copyright (c) 2024, pankaj.sharma@vcm.org.in and contributors
 // For license information, please see license.txt
-
-// frappe.ui.form.on("VCM Item Code Printer", {
-// 	refresh(frm) {
-
-// 	},
-// });
-// Copyright (c) 2023, Narahari Dasa and contributors
-// For license information, please see license.txt
-
 frappe.ui.form.on("VCM Item Code Printer", {
     refresh(frm) {
-      frm.add_custom_button(
-        __("Print Expiry Label"),
-        async function () {
-          if (
-            frm.doc.item_code &&
-            frm.doc.company &&
-            frm.doc.price_list &&
-            frm.doc.quantity_exp
-          ) {
-            console.log(frm.doc.item_code);
-            frappe.ui.form
-              .qz_connect()
-              .then(function () {
-                var config = qz.configs.create("TE210"); // Exact printer name from OS
-                var print_data = [];
-                var shift = 250; // Adjust shift for three stickers per row
-  
-                frappe.call({
-                  method:
-                    "vcm.erpnext_vcm.godex_print.filter_sellable_items",
-                  args: {
-                    items: [frm.doc.item_code],
-                    price_list: frm.doc.price_list,
-                    company: frm.doc.company,
-                  },
-                  freeze: true,
-                  callback: (r) => {
-                    console.log(r.message);
-                    var items_detailed = r.message;
-                    var total_items = [];
-                    var qty = frm.doc.quantity_exp;
-  
-                    var mfg_date = new Date(frm.doc.manufacturing_date);
-                    var expiry_date = addDaysToDate(
-                      mfg_date,
-                      frm.doc.day_after_expiry
-                    );
-  
-                    mfg_date = formatDateToDMY(mfg_date);
-                    expiry_date = formatDateToDMY(expiry_date);
-  
-                    for (var i = 0; i < qty; i++) {
-                      total_items.push(items_detailed[0]);
+      frm.add_custom_button(__("Print Item Codes"), function(){
+        frappe.ui.form.qz_connect()
+      .then(function () {
+        var config = qz.configs.create("Godex G500");            // Exact printer name from OS
+        var print_data =[];
+        var shift =310;
+
+        var stock_qty ={}
+        #console.log("Print Item code items:", frm.doc.items);
+        for(var index in frm.doc.items){
+          stock_qty[frm.doc.items[index]['item_code']] = frm.doc.items[index]['qty'];
+          #console.log("Print Item code stock_qty:", index, frm.doc.items[index]['qty'], stock_qty[frm.doc.items[index]['item_code']] );
+        } 
+        #console.log("Print Item code 2 stock qty:", stock_qty);
+
+        #console.log("Print Item code just before frappe call:");
+        frappe.call({
+          method:
+            "vcm.erpnext_vcm.godex_print.filter_sellable_items",
+          args: {
+            items: frm.doc.items
+              .map(a => a['item_code']),
+            price_list:frm.doc.price_list,
+            company:frm.doc.company                                                      
+          },  
+          freeze: true,
+          callback: (r) => {
+            console.log(r.message);
+            #console.log(stock_qty)
+            var items_detailed = r.message;
+            var total_items =[];
+            #console.log("Print Item code in callback:");
+            //based upon number of items prepare rows
+            for(var index in items_detailed){
+                // get quantity of each item based upon code
+                var qty = stock_qty[items_detailed[index]['code']];
+                // for each item push number of rows based upon qty
+                if(items_detailed[index]['name']!=null){
+                    for(var i=0;i<qty;i++){
+                        total_items.push({
+                            "name" : items_detailed[index]['name'],
+                            "code" : items_detailed[index]['code'],
+                            "rate" : items_detailed[index]['rate'],
+                        });
                     }
-  
-                    var group_string = "^XA";
-                    var item_processed = 0;
-  
-                    for (var j = 0; j < qty; j += 3) {
-                      for (var g = 0; g < 3; g++) {
-                        if (j + g < total_items.length) {
-                          if (item_processed == qty) {
-                            break;
-                          }
-  
-                         var xOffset = 15 + (g * 20) + (g * shift);
-                          var yOffset = 20;
-                          var itemCode = total_items[j + g]["code"];
-                       
-  
-                          group_string += `^FO${xOffset+2},${yOffset} ^BY 2,1,100 ^BCN,,N,N,N ^FD${itemCode}^FS`;
-                          group_string += `^FO${xOffset},${
-                            yOffset + 5 + 105
-                          }^FB200,3,5,L^A0N,30,17 ^FD${
-                          ""
-                          }\n^FS`;
-                          group_string += `^FO${xOffset},${
-                            yOffset + 5 + 108
-                          }^FB280,3,5,L^A0N,30,19 ^FD${
-                            "ITEM CODE:" + total_items[j + g]["code"]
-                          }\n^FS`;
-                          
-                          group_string += `^FO${xOffset},${
-                            yOffset + 5 + 145
-                          }^FB200,3,5,L^A0N,30,20 ^FD${"MFG:" + mfg_date}\n${"EXP:" + expiry_date}^FS`;
-  
-                          item_processed++;
-                        }
-                      }
-                      if (item_processed == qty) {
-                        break;
-                      }
-                      group_string += "^XZ\n^XA";
-                    }
-                    group_string += "^XZ";
-                    print_data.push(group_string);
-  
-                    return qz.print(config, print_data);
-                  },
-                  error: (r) => {
-                    // on error
-                  },
-                });
-              })
-              .then(frappe.ui.form.qz_success)
-              .catch((err) => {
-                frappe.ui.form.qz_fail(err);
-              });
-            // frm.events.on_success(frm);
-          } else {
-            frappe.throw("Fields for printing Item Labels are not available.");
-          }
-  
-          // Perform desired action such as routing to new form or fetching etc.
-        },
-        __("Utilities")
-      );
-      frm.add_custom_button(
-        __("Print Item Codes"),
-        async function () {
-          if (
-            frm.doc.item_code &&
-            frm.doc.company &&
-            frm.doc.price_list &&
-            frm.doc.quantity
-          ) {
-            console.log(frm.doc.item_code);
-            frappe.ui.form
-              .qz_connect()
-              .then(function () {
-                var config = qz.configs.create("Godex G500"); // Exact printer name from OS
-                var print_data = [];
-                var shift = 310;
-                frappe.call({
-                  method:
-                    "vcm.erpnext_vcm.godex_print.filter_sellable_items",
-                  args: {
-                    items: [frm.doc.item_code],
-                    price_list: frm.doc.price_list,
-                    company: frm.doc.company,
-                  },
-  
-                  freeze: true,
-                  callback: (r) => {
-                    console.log(r.message);
-                    var items_detailed = r.message;
-                    var total_items = [];
-                    var qty = frm.doc.quantity;
-                    console.log(qty);
-                    for (var i = 0; i < qty; i++) {
-                      total_items.push(items_detailed[0]);
-                    }
-  
-                    console.log(total_items);
-                    for (var j = 0; j < total_items.length; j = j + 2) {
-                      var group_string = "";
-                      for (var g = 0; g < 2; g++) {
-                        if (j + g < total_items.length) {
-                          group_string +=
-                            `\nBA3,` +
-                            (4 + g * shift) +
-                            `,8,1,2,63,0,0,` +
-                            total_items[j + g]["code"] +
-                            `\nAB,` +
-                            (4 + g * shift) +
-                            `,70,1,1,0,0E,` +
-                            total_items[j + g]["code"] +
-                            `\nAA,` +
-                            (4 + g * shift) +
-                            `,104,1,1,0,0E,` +
-                            total_items[j + g]["name"] +
-                            `\nAC,` +
-                            (4 + g * shift) +
-                            `,131,1,1,0,0E,Rs.` +
-                            total_items[j + g]["rate"];
-                        }
-                      }
-                      print_data.push(
-                        frm.events.get_EZPL_string(frm, group_string)
-                      );
-                    }
-                    return qz.print(config, print_data);
-                  },
-                  error: (r) => {
-                    // on error
-                  },
-                });
-              })
-              .then(frappe.ui.form.qz_success)
-              .catch((err) => {
-                frappe.ui.form.qz_fail(err);
-              });
-            // frm.events.on_success(frm);
-          } else {
-            frappe.throw("Fields for printing Item Labels are not available.");
-          }
-  
-          //perform desired action such as routing to new form or fetching etc.
-        },
-        __("Utilities")
-      );
-      frm.add_custom_button(
-        __("Print Dates"),
-        function () {
-          if (frm.doc.date && frm.doc.quantity_date) {
-            frappe.ui.form
-              .qz_connect()
-              .then(function () {
-                var config = qz.configs.create("Godex G500"); // Exact printer name from OS
-                var print_data = [];
-                var shift = 310;
-                for (var j = 0; j < frm.doc.quantity_date; j = j + 2) {
-                  var group_string = "";
-                  for (var g = 0; g < 2; g++) {
-                    if (j + g < frm.doc.quantity_date) {
-                      group_string +=
-                        `\nAB,` +
-                        (4 + g * shift) +
-                        `,50,2,2,0,0E,` +
-                        frappe.format(frm.doc.date, { fieldtype: "Date" });
-                    }
-                  }
-                  print_data.push(frm.events.get_EZPL_string(frm, group_string));
                 }
-                return qz.print(config, print_data);
-              })
-              .then(frappe.ui.form.qz_success)
-              .catch((err) => {
-                frappe.ui.form.qz_fail(err);
-              });
-            // frm.events.on_success(frm)
-          } else {
-            frappe.throw("Fields for printing Date Labels are not available.");
-          }
-        },
-        __("Utilities")
-      );
+              }  
+            #console.log("****Print Item code total_items:", total_items);            
+            for (var j = 0; j < total_items.length; j = j + 2) {
+              var group_string = "";
+              for (var g = 0; g < 2; g++) {
+                if (j + g < total_items.length) {
+                  group_string +=
+                    `\nBA3,` +
+                    (4 + g * shift) +
+                    `,8,1,2,63,0,0,` +
+                    total_items[j + g]["code"] +
+                    `\nAB,` +
+                    (4 + g * shift) +
+                    `,70,1,1,0,0E,` +
+                    total_items[j + g]["code"] +
+                    `\nAA,` +
+                    (4 + g * shift) +
+                    `,104,1,1,0,0E,` +
+                    total_items[j + g]["name"] +
+                    `\nAC,` +
+                    (4 + g * shift) +
+                    `,131,1,1,0,0E,Rs.` +
+                    total_items[j + g]["rate"];
+                }
+              }
+              #console.log("****Print Item code before print push");
+              print_data.push(
+                frm.events.get_EZPL_string(frm, group_string)
+              );
+            }
+            return qz.print(config, print_data);
+          },
+          error: (r) => {
+            frappe.throw("Print error from qz failed.");
+            }
+        });
+      })
+      
+      .then(frappe.ui.form.qz_success)
+      .catch((err) => {
+        frappe.ui.form.qz_fail(err);
+        frappe.throw("Print error on qz error.");
+      });      
+    }, __("Utilities"));
+      //perform desired action such as routing to new form or fetching etc.
     },
+      
+    
   
     on_success: function (frm) {
       frm.set_value("item_code", null);
@@ -271,19 +128,8 @@ frappe.ui.form.on("VCM Item Code Printer", {
         final_data += arr_data[index].trim() + "\x0D";
       }
       return final_data;
-    },
-  });
-  function formatDateToDMY(date) {
-    return date.toLocaleDateString("en-GB", {
-      day: "2-digit",
-      month: "2-digit",
-      year: "2-digit",
-    });
-  }
-  function addDaysToDate(date, daysToAdd) {
-    let newDate = new Date(date); // Clone the original date
-    newDate.setDate(newDate.getDate() + daysToAdd);
-    return newDate;
-  }
+    }
+})
+
   
   
--- vcm/erpnext_vcm/godex_print.py
+++ vcm/erpnext_vcm/godex_print.py
@@ -1,37 +1,56 @@
 import frappe,requests,json
 
-#import logging
-#logging.basicConfig(level=logging.DEBUG)
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 @frappe.whitelist()
 def filter_sellable_items(items,price_list,company):
-	#logging.debug(f"in Filter sellable items {items}. {price_list}. {company}") 
-	items = json.loads(items)
-	#logging.debug(f"in Filter sellable items1 {items}") 
-	
-	item_codes = tuple(items)
-	#logging.debug(f"in Filter sellable items2{item_codes}" )
-	join_item = ','.join(["'"+str(item)+"'" for item in items])
-	join_item = "({})".format(join_item)
+        #logging.debug(f"in Filter sellable items .")
+        #logging.debug(f"in Filter sellable items . {price_list}. {company}") 
+        try:
+            items = json.loads(items)
+        except json.JSONDecodeError:
+            frappe.throw(_("Invalid items parameter format. Please provide a valid JSON array."))
+        
+        # Ensure items is a list
+        if not isinstance(items, list):
+            frappe.throw(_("Items must be a list."))
+        
+        #logging.debug(f"in Filter sellable items1 {items}") 
+        
+        # Convert the list to a SQL-safe string
+        items_tuple = tuple(items)
+        #logging.debug(f"in Filter sellable items3 {items_tuple}")
 
-	query ="""
-			SELECT
-				item.`item_code` as code,
-				item.`item_name` as name,
-				IF(STRCMP(item_template.name,"")=1,
-					ROUND(item_price.`price_list_rate`+(item_price.`price_list_rate`* item_template.cumulative_tax/100),0), 
-					ROUND(item_price.`price_list_rate`,0)
-				) as rate
-			FROM
-				`tabItem` item
-			LEFT JOIN `tabItem Price` item_price
-				ON item_price.item_code = item.item_code
-			LEFT JOIN `tabItem Tax` tax
-				ON tax.parent = item.name
-			LEFT JOIN `tabItem Tax Template` item_template
-				ON tax.item_tax_template = item_template.name
-			WHERE item.has_variants=0 and item.name IN {} and item_price.price_list = '{}' and (item_template.name is null or item_template.company = '{}')
-			GROUP BY item.name
-			""".format(join_item,price_list,company)
-	data = frappe.db.sql(query,as_dict=1)
-	return data
+        # Prepare the query
+        query = """
+            SELECT
+                item.`item_code` AS code,
+                item.`item_name` AS name,
+                IF(STRCMP(item_template.name, "") = 1,
+                    ROUND(item_price.`price_list_rate` + (item_price.`price_list_rate` * item_template.cumulative_tax / 100), 0),
+                    ROUND(item_price.`price_list_rate`, 0)
+                ) AS rate
+            FROM
+                `tabItem` item
+            LEFT JOIN `tabItem Price` item_price
+                ON item_price.item_code = item.item_code
+            LEFT JOIN `tabItem Tax` tax
+                ON tax.parent = item.name
+            LEFT JOIN `tabItem Tax Template` item_template
+                ON tax.item_tax_template = item_template.name
+            WHERE item.has_variants = 0
+                AND item.name IN %(items)s
+                AND item_price.price_list = %(price_list)s
+                AND (item_template.name IS NULL OR item_template.company = %(company)s)
+            GROUP BY item.name
+            """
+
+        # Execute the query
+        data = frappe.db.sql(query, {
+            'items': items_tuple,
+            'price_list': price_list,
+            'company': company
+        }, as_dict=True)
+        #logging.debug(f"in Filter sellable items6 {data}")
+        return data
