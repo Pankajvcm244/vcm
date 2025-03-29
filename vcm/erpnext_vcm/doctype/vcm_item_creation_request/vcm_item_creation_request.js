78dfbbeb8a750fe0faeb1b19701eff42e8387b75
pankajvcm244 <pankajvcm244@gmail.com>
2025-03-29 14:52:32 +0530

    print format and item creation doc, KP foc layout

---
+++ vcm/erpnext_vcm/doctype/vcm_item_creation_request/vcm_item_creation_request.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, pankaj.sharma@vcm.org.in and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("VCM Item Creation Request", {
+// 	refresh(frm) {
+
+// 	},
+// });
