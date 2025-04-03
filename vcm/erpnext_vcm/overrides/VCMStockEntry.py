3cc132e6bc241be63145b0c5b41d67295b504c6c
pankajvcm244 <pankajvcm244@gmail.com>
2025-04-03 12:05:39 +0000

    changed series to variable fiscal year

--- vcm/erpnext_vcm/overrides/VCMStockEntry.py
+++ vcm/erpnext_vcm/overrides/VCMStockEntry.py
@@ -5,14 +5,21 @@ from frappe.model.docstatus import DocStatus
 from frappe.utils import flt
 from frappe.utils.data import getdate
 from frappe.model.naming import getseries
+import datetime
 
 class VCMStockEntry(StockEntry):
     def autoname(self):
         # based upon creation date
         company_abbr = frappe.get_cached_value("Company", self.company, "abbr")
-        #year = dateF.strftime("%y")
-        #month = dateF.strftime("%m")
-        prefix = f"{company_abbr}-STE-2526-"
+        postingdate = datetime.datetime.now()
+        postingyear = postingdate.year
+        postingmonth = postingdate.month
+         # Determine fiscal year
+        if postingmonth < 4:  # If before April, it's part of the previous fiscal year
+            fiscal_year = f"{(postingyear-1)% 100}{postingyear % 100}"
+        else:
+            fiscal_year = f"{postingyear % 100}{(postingyear + 1) % 100}"
+        prefix = f"{company_abbr}-STE-{fiscal_year}-"
         self.name = prefix + getseries(prefix, 6)
 
 
--- vcm/erpnext_vcm/overrides/VCMStockRecon.py
+++ vcm/erpnext_vcm/overrides/VCMStockRecon.py
@@ -1,14 +1,23 @@
 import frappe
 from frappe.model.naming import getseries
 from erpnext.stock.doctype.stock_reconciliation.stock_reconciliation import StockReconciliation
-
+from frappe.utils.data import getdate
+from frappe.model.naming import getseries
+import datetime
 
 class VCMStockRecon(StockReconciliation):
     
     def autoname(self):
         # based upon creation date
-        company_abbr = frappe.get_cached_value("Company", self.company, "abbr")
-        #year = dateF.strftime("%y")
-        #month = dateF.strftime("%m")
-        prefix = f"{company_abbr}-MAT-RECO-2526-"
+        company_abbr = frappe.get_cached_value("Company", self.company, "abbr")   
+        #postingdate = getdate(self.posting_date)
+        postingdate = datetime.datetime.now()
+        postingyear = postingdate.year
+        postingmonth = postingdate.month
+         # Determine fiscal year
+        if postingmonth < 4:  # If before April, it's part of the previous fiscal year
+            fiscal_year = f"{(postingyear-1)% 100}{postingyear % 100}"
+        else:
+            fiscal_year = f"{postingyear % 100}{(postingyear + 1) % 100}"
+        prefix = f"{company_abbr}-MAT-RECO-{fiscal_year}-"
         self.name = prefix + getseries(prefix, 5)
