4c6c7f928242978f9d31524c4094063ed05dc4a9
pankajvcm244 <pankajvcm244@gmail.com>
2025-04-18 11:38:38 +0000

    added small label print and fixed PO override typo

--- vcm/public/js/purchase_receipt.js
+++ vcm/public/js/purchase_receipt.js
@@ -77,7 +77,95 @@ frappe.ui.form.on('Purchase Receipt', {
                 }
             };
         };
-        frm.add_custom_button(__("Print VCM Labels directly"), function(){
+        frm.add_custom_button(__("Print Small VCM Labels directly"), function(){
+            frappe.ui.form.qz_connect()
+            .then(function () {
+                var config = qz.configs.create("Godex G500");            // Exact printer name from OS
+                var print_data =[];
+                var shift =200;
+
+                var stock_qty ={}
+                for(var index in frm.doc.items){
+                    stock_qty[frm.doc.items[index]['item_code']] = frm.doc.items[index]['qty'];
+                }
+                //console.log(frm.doc.items);
+                //frm.doc.items.forEach(a => console.log(a['do_not_print_in_barcodes']));
+
+                let d = new frappe.ui.Dialog({
+                                title: 'Select Price List',
+                                fields: [
+                                    {
+                                        label: 'Price List',
+                                        fieldname: 'price_list',
+                                        fieldtype: 'Link',
+                                        options:'Price List'
+                                    }
+                                ],
+                                primary_action_label: 'Print',
+                                primary_action(values) {
+                                    frappe.call({
+                                        method: 'vcm.erpnext_vcm.godex_print.filter_sellable_items',
+                                        args: {
+                                            //items: frm.doc.items.filter(a => a['do_not_print_in_barcodes']==false).map(a => a['item_code']),
+                                            items: frm.doc.items
+                                                //.filter(a => a['do_not_print_in_barcodes'] == false)
+                                                .map(a => a['item_code']),
+                                            company:frm.doc.company,
+                                            price_list:values.price_list
+                                        },
+                                        freeze: true,
+                                        callback: (r) => {
+                                            //console.log(r.message);
+                                            //console.log(stock_qty)
+                                            var items_detailed = r.message;
+                                            var total_items =[];
+                                            for(var index in items_detailed){
+                                                var qty = stock_qty[items_detailed[index]['code']];
+                                                if(items_detailed[index]['name']!=null){
+                                                    for(var i=0;i<qty;i++){
+                                                        total_items.push({
+                                                            "name" : items_detailed[index]['name'],
+                                                            "code" : items_detailed[index]['code'],
+                                                            "rate" : items_detailed[index]['rate'],
+                                                        });
+                                                    }
+                                                }
+                                            }
+                                            //console.log(total_items);
+                                            for(var j=0; j<total_items.length; j=j+4){
+                                                var group_string="";
+                                                    for(var g=0; g< 4; g++){
+                                                        if(j + g < total_items.length){
+                                                            group_string +=
+                                                                `\nBA3,`+ (6 + (g * shift)) + `,10,1,1,40,0,0,` + total_items[j + g]["code"] +  
+                                                                `\nAA,` + (6 + (g * shift)) + `,55,1,1,0,0E,` + total_items[j + g]["code"] +
+                                                                `\nAB,` + (6 + (g * shift)) + `,75,1,1,0,0E,Rs.` + total_items[j + g]["rate"];
+                                                        }
+                                                    }
+                                                    print_data.push(frm.events.get_EZPL_string_small(frm,group_string));
+                                                }
+                                            return qz.print(config, print_data)
+                                        },
+                                        error: (r) => {
+                                            // on error
+                                        }
+                                    })
+                                    d.hide();
+                                }
+                            });
+                
+                d.show();
+
+            })
+            .then(frappe.ui.form.qz_success)
+            .catch(err => {
+                frappe.ui.form.qz_fail(err);
+                frappe.throw("Print error on qz error.");
+            });
+            //perform desired action such as routing to new form or fetching etc.
+        }, __("Utilities"));
+        
+        frm.add_custom_button(__("Print Large VCM Labels directly"), function(){
             frappe.ui.form.qz_connect()
             .then(function () {
                 var config = qz.configs.create("Godex G500");            // Exact printer name from OS
@@ -160,6 +248,7 @@ frappe.ui.form.on('Purchase Receipt', {
             .then(frappe.ui.form.qz_success)
             .catch(err => {
                 frappe.ui.form.qz_fail(err);
+                frappe.throw("Print error on qz error.");
             });
             //perform desired action such as routing to new form or fetching etc.
         }, __("Utilities"));
@@ -192,6 +281,35 @@ frappe.ui.form.on('Purchase Receipt', {
         var final_data = "";
         for(var index in arr_data){
             final_data +=  arr_data[index].trim() + "\x0D";}
+        return final_data;
+    },
+
+    get_EZPL_string_small: function (frm, data) {
+        // Please refer to documentation for seetings : https://www.godexprinters.co.uk/downloads/manuals/desktop/EZPL_EN_J_20180226.pdf
+        var settings = `
+                            ^Q15,3 
+                            ^W100    
+                            ^H10   
+                            ^P1   
+                            ^S2    
+                            ^AD  
+                            ^C1   
+                            ^R8
+                            ~Q+0   
+                            ^O0   
+                            ^D0    
+                            ^E18     
+                            ~R255   
+                            ^L      
+                            `;
+        var end = `\nE`;
+        var str = settings + data + end;
+        // Split into lines and append carriage return at the end of each line
+        var arr_data = str.split("\n");
+        var final_data = "";
+        for (var index in arr_data) {
+          final_data += arr_data[index].trim() + "\x0D";
+        }
         return final_data;
     }
 })
