807620272c1218ef5c73077267596efb51be4588
pankajvcm244 <pankajvcm244@gmail.com>
2025-05-14 13:00:16 +0000

    made function of notifying users on payment entry

--- vcm/erpnext_vcm/overrides/VCMPaymentEntry.py
+++ vcm/erpnext_vcm/overrides/VCMPaymentEntry.py
@@ -43,46 +43,9 @@ class VCMPaymentEntry(PaymentEntry):
                 if validate_budget_head_n_location_mandatory(self) == True:
                     #logging.debug(f"VCM PE on_Submit -2 ")
                     update_vcm_budget_on_payment_submit(self, True)            
-
-        vcm_whatsapp_settings = frappe.get_doc("VCM WhatsAPP Settings") 
-        #logging.debug(f"VCM Payment Entry after_submit {vcm_whatsapp_settings.payment_entry_whatsapp_enabled} ")
-        # this  Flag payment_entry_whatsapp_enabled is for Purchase person
-        if vcm_whatsapp_settings.payment_entry_whatsapp_enabled:
-            purchase_person_email = self.custom_purchase_person 
-            mobile_no = frappe.db.get_value("User", {"email": purchase_person_email}, "mobile_no")  # Get mobile from Email 
-            purchase_person_name = frappe.db.get_value("User", {"email": purchase_person_email}, "first_name")  # Get mobile from Email                     
-            #logging.debug(f"VCM Payment Entry after_submit {purchase_person_email}, {mobile_no}")
-            if purchase_person_email is None:
-                #logging.debug(f"No Purchase user defined for Payment Entry {self.name}")
-                return
-            else:
-                #mobile_no = frappe.get_value("User", user, "mobile_no")
-                #purchase_person_email = frappe.get_value("User", user, "email")
-                if mobile_no:
-                    #logging.debug(f"VCM Payment Entry after_submit calling send whatsapp {purchase_person_name},{mobile_no}")
-                    send_whatsapp_to_person(self, mobile_no, purchase_person_name)
-                if purchase_person_email:
-                    #logging.debug(f"VCM Payment Entry after_submit calling send email {purchase_person_name},{purchase_person_email}")
-                    send_email_to_person(self, purchase_person_email, purchase_person_name) 
-        # this  Flag payment_entry_whatsapp_enabled is for Supplier email and whatsup
-        if vcm_whatsapp_settings.supplier_whatsapp_enabled:
-            vcm_party_name = self.party_name             
-            if vcm_party_name is None:
-                #logging.debug(f"No Supplier is defined for Payment Entry {self.name}")
-                return
-            else:
-                billing_address_name = f"{vcm_party_name}-Billing"
-                supplier_mobile = frappe.db.get_value("Address", {"name": billing_address_name}, "phone")
-                supplier_emailid = frappe.db.get_value("Address", {"name": billing_address_name}, "email_id")
-                #logging.debug(f"VCM Payment Entry after_submit {billing_address_name}, {supplier_mobile}, {supplier_emailid}")
-                if supplier_mobile:
-                    #logging.debug(f"VCM Payment Entry 2 after_submit calling send whatsapp {vcm_party_name},{supplier_mobile}")
-                    send_whatsapp_to_person(self, supplier_mobile, vcm_party_name)
-                if supplier_emailid:
-                    #logging.debug(f"VCM Payment Entry  2 after_submit calling send email {self},{supplier_emailid}")
-                    send_email_to_person(self, supplier_emailid, vcm_party_name) 
-         
-
+        notify_purchase_person_on_payment_entry(self)
+        notify_supplier_person_on_payment_entry(self)
+                
     def on_cancel(self):
         super().on_cancel()
         vcm_budget_settings = frappe.get_cached_doc("VCM Budget Settings")
@@ -95,8 +58,6 @@ class VCMPaymentEntry(PaymentEntry):
                 #if validate_budget_head_n_location_mandatory(self) == True:
                 update_vcm_budget_on_payment_submit(self, False) 
                     
-
-
     def validate(self):
         super().validate()
         #logging.debug(f"VCM PE validate")
@@ -108,6 +69,49 @@ class VCMPaymentEntry(PaymentEntry):
                     validate_vcm_budget_on_payment_entry(self)
         return
 
+
+
+
+def notify_purchase_person_on_payment_entry(self):
+        vcm_whatsapp_settings = frappe.get_doc("VCM WhatsAPP Settings") 
+        #logging.debug(f"VCM Payment Entry after_submit {vcm_whatsapp_settings.payment_entry_whatsapp_enabled} ")
+        # this  Flag payment_entry_whatsapp_enabled is for Purchase person
+        if vcm_whatsapp_settings.payment_entry_whatsapp_enabled:
+            purchase_person_email = self.custom_purchase_person 
+            mobile_no = frappe.db.get_value("User", {"email": purchase_person_email}, "mobile_no")  # Get mobile from Email 
+            purchase_person_name = frappe.db.get_value("User", {"email": purchase_person_email}, "first_name")  # Get mobile from Email                     
+            #logging.debug(f"VCM Payment Entry after_submit {purchase_person_email}, {mobile_no}")
+            if purchase_person_email is None:
+                logging.debug(f"No Purchase user defined for Payment Entry {self.name}")                
+            else:
+                #mobile_no = frappe.get_value("User", user, "mobile_no")
+                #purchase_person_email = frappe.get_value("User", user, "email")
+                if mobile_no:
+                    #logging.debug(f"VCM Payment Entry after_submit calling send whatsapp {purchase_person_name},{mobile_no}")
+                    send_whatsapp_to_person(self, mobile_no, purchase_person_name)
+                if purchase_person_email:
+                    #logging.debug(f"VCM Payment Entry after_submit calling send email {purchase_person_name},{purchase_person_email}")
+                    send_email_to_person(self, purchase_person_email, purchase_person_name) 
+
+
+def notify_supplier_person_on_payment_entry(self):
+        vcm_whatsapp_settings = frappe.get_doc("VCM WhatsAPP Settings") 
+        # this  Flag payment_entry_whatsapp_enabled is for Supplier email and whatsup
+        if vcm_whatsapp_settings.supplier_whatsapp_enabled:
+            vcm_party_name = self.party_name             
+            if vcm_party_name is None:
+                logging.debug(f"No Supplier is defined for Payment Entry {self.name}")                
+            else:
+                billing_address_name = f"{vcm_party_name}-Billing"
+                supplier_mobile = frappe.db.get_value("Address", {"name": billing_address_name}, "phone")
+                supplier_emailid = frappe.db.get_value("Address", {"name": billing_address_name}, "email_id")
+                #logging.debug(f"VCM Payment Entry after_submit {billing_address_name}, {supplier_mobile}, {supplier_emailid}")
+                if supplier_mobile:
+                    #logging.debug(f"VCM Payment Entry 2 after_submit calling send whatsapp {vcm_party_name},{supplier_mobile}")
+                    send_whatsapp_to_person(self, supplier_mobile, vcm_party_name)
+                if supplier_emailid:
+                    #logging.debug(f"VCM Payment Entry  2 after_submit calling send email {self},{supplier_emailid}")
+                    send_email_to_person(self, supplier_emailid, vcm_party_name) 
 
 def send_email_to_person(self, person_email, person_name):
         #logging.debug(f"**in send_email_to_purchase_person  {self.name} , {person_email} ************* ")
